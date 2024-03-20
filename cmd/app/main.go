860b4f467893b60eb6c5fccf87ec7ec20dda1920
k-engineer-ch <k.engineer.ch2023@gmail.com>
2024-03-20 12:31:41 +0900

    サンプルAPIを追加

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(">>> Start API")
+
+	r := gin.Default()
+
+	r.GET("/sample", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "サンプル API",
+			"int":     1,
+			"map": map[string]int{
+				"key1": 100,
+				"key2": 200,
+			},
+		})
+	})
+	r.Run()
 }
