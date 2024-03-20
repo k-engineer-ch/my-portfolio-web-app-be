907ac7cfdcf46d53719cdb8e815167c144afe173
k-engineer-ch <k.engineer.ch2023@gmail.com>
2024-03-20 13:01:57 +0900

    環境変数取得処理確認

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/caarlos0/env/v10"
 	"github.com/gin-gonic/gin"
 )
 
+type config struct {
+	supabaseUrl string `env:"SUPABASE_URL"`
+	supabaseKey string `env:"SUPABASE_KEY"`
+}
+
 func main() {
 	fmt.Println(">>> Start API")
+
+	// supabaseUrl := "https://glfgqyvbbqkpxrxizccv.supabase.co"
+	// supabaseKey := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsZmdxeXZiYnFrcHhyeGl6Y2N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA5MDQyNDgsImV4cCI6MjAyNjQ4MDI0OH0.PFX1AZP2DJwgQWrqI3zNJDi9WUs8UpIJS2fRZzol8Uo"
+	// supabase := supa.CreateClient(supabaseUrl, supabaseKey)
+
+	// var results map[string]interface{}
+	// err := supabase.DB.From("countries").Select("*").Single().Execute(&results)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(results) // Selected rows
+
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	fmt.Printf("%+v\n", cfg)
 
 	r := gin.Default()
 
