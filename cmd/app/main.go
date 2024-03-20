062c0e47022c77c616c5e05c8575003cbf44f562
k-engineer-ch <k.engineer.ch2023@gmail.com>
2024-03-20 14:52:58 +0900

    supabaseとの接続挑戦中

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,45 +2,71 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	supa "github.com/nedpals/supabase-go"
+	"github.com/joho/godotenv"
+	// supa "github.com/nedpals/supabase-go"
+	supabase "github.com/supabase-community/supabase-go"
 )
+
+type Expense struct {
+}
 
 func main() {
 	fmt.Println(">>> Start API")
+
+	// .envファイルから環境変数を読み込む
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	// Supabaseへの接続情報「Project URL」を環境変数から取得
 	supabaseUrl := os.Getenv("SUPABASE_URL")
 	// Supanaseへの接続情報「Project API keys」を環境変数から取得
 	supabaseKey := os.Getenv("SUPABASE_KEY")
-
 	// Supabaseへの接続クライアント作成
-	supabase := supa.CreateClient(supabaseUrl, supabaseKey)
+	// supabase := supa.CreateClient(supabaseUrl, supabaseKey)
 
-	// Select処理（サンプル）
-	var results map[string]interface{}
-	err := supabase.DB.From("expenses").Select("*").Single().Execute(&results)
+	fmt.Println("Project URL:", supabaseUrl)
+	fmt.Println("Project API keys:", supabaseKey)
+
+	client, err := supabase.NewClient(supabaseUrl, supabaseKey, nil)
 	if err != nil {
-		panic(err)
+		fmt.Println("cannot initalize client", err)
+	}
+	// data, count, err := client.From("expenses").Select("", "exact", false).Execute()
+	data, count, err := client.From("expenses").Select("*", "exact", false).Execute()
+
+	if err != nil {
+		fmt.Println("sql error", err)
+	} else {
+		fmt.Println(data)
+		fmt.Println(count)
 	}
 
-	// 検索結果を出力
-	fmt.Println(results)
+	// // Select処理（サンプル）
+	// var results map[string]interface{}
+	// err := supabase.DB.From("expenses").Select("*").Single().Execute(&results)
+	// // err := supabase.DB.From("expenses").Select("*").Execute(&results)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	r := gin.Default()
+	// // 検索結果を出力
+	// fmt.Println(results)
 
-	r.GET("/sample", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "サンプル API",
-			"int":     1,
-			"map": map[string]int{
-				"key1": 100,
-				"key2": 200,
-			},
-		})
-	})
-	r.Run()
+	// r := gin.Default()
+
+	// r.GET("/sample", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "サンプル API",
+	// 		"int":     1,
+	// 		"map": map[string]int{
+	// 			"key1": 100,
+	// 			"key2": 200,
+	// 		},
+	// 	})
+	// })
+	// r.Run()
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -24,6 +25,8 @@ require (
 	github.com/nedpals/postgrest-go v0.1.3 // indirect
 	github.com/nedpals/supabase-go v0.4.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.0 // indirect
+	github.com/supabase-community/supabase-go v0.0.1 // indirect
+	github.com/supabase/postgrest-go v0.0.7 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
